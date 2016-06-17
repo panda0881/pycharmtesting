576a39dbf82a6d983a59bf48ab0d9d8e14eab9d7
Hongming <hzhangal0330@gmail.com>
2016-06-17 17:30:45 +0800

    testing

--- testing0.py
+++ testing0.py
@@ -1,12 +1,11 @@
-from pil import Image
+from PIL import Image
 
-import argparse
-
-ascii_char = list("!@#$%^&*()12345670QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm")
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 WIDTH = 80
 HEIGHT = 80
 
-def get_char(r,g,b,alpha = 256):
+
+def get_char(r, g, b, alpha = 256):
     if alpha == 0:
         return ' '
     length = len(ascii_char)
@@ -14,8 +13,16 @@ def get_char(r,g,b,alpha = 256):
     unit = (256.0+1)/length
     return ascii_char[int(gray/unit)]
 
+
 if __name__ == '__main__':
     im=Image.open("ascii_dora.png")
     im=im.resize((WIDTH,HEIGHT),Image.NEAREST)
 
     txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j, i)))
+        txt += '\n'
+
+    print(txt)
