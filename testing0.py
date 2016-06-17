82611a3e16784da197c98ba8f74344574c3add8d
Hongming <hzhangal0330@gmail.com>
2016-06-17 17:37:28 +0800

    testing

--- testing0.py
+++ testing0.py
@@ -5,7 +5,7 @@ WIDTH = 80
 HEIGHT = 80
 
 
-def get_char(r, g, b, alpha = 256):
+def get_char(r, g, b, alpha=256):
     if alpha == 0:
         return ' '
     length = len(ascii_char)
@@ -14,15 +14,14 @@ def get_char(r, g, b, alpha = 256):
     return ascii_char[int(gray/unit)]
 
 
-if __name__ == '__main__':
-    im=Image.open("ascii_dora.png")
-    im=im.resize((WIDTH,HEIGHT),Image.NEAREST)
+im = Image.open("ascii_dora.png")
+im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
 
-    txt = ""
+txt = ""
 
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
-            txt += get_char(*im.getpixel((j, i)))
-        txt += '\n'
+for i in range(HEIGHT):
+    for j in range(WIDTH):
+        txt += get_char(*im.getpixel((j, i)))
+    txt += '\n'
 
-    print(txt)
+print(txt)
