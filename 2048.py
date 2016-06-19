be434866b17a37c5effedc47f66a5bd551e2d109
Hongming <hzhangal0330@gmail.com>
2016-06-19 15:50:29 +0800

    testing

--- 2048.py
+++ 2048.py
@@ -11,6 +11,32 @@ letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
 actions_dict = dict((zip(letter_codes, actions*2)))
 
 
+def move_row_left(row):
+    def tighten(row):
+        new_row = [i for i in row if i != 0]
+        new_row += [0 for i in range(len(row) - len(new_row))]
+        return new_row
+
+    def merge(row):
+        pair = False
+        new_row = []
+        for i in range(len(row)):
+            if pair:
+                new_row.append(2 * row[i])
+                self.score += 2 * row[i]
+                pair = False
+            else:
+                if i + 1 < len(row) and row[i] == row[i + 1]:
+                    pair = True
+                    new_row.append(0)
+                else:
+                    new_row.append(row[i])
+        assert len(new_row) == len(row)
+        return new_row
+
+    return tighten(merge(tighten(row)))
+
+
 def get_user_action(keyboard):
     char = "N"
     while char not in actions_dict:
@@ -43,6 +69,17 @@ class GameField(object):
             new_element = 2
         (i,j) = choice([(i,j) for i in range(self.width) for j in range(self.height) if self.field[i][j] == 0])
         self.field[i][j] = new_element
+
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.field = [[0 for i in range(self.width)] for j in range(self.height)]
+        self.spawn()
+
+
+
 
 
 
