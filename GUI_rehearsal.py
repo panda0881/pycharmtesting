f2c87cf8b21e43564fc8d8b6b87468f680deb756
Hongming <hzhangal0330@gmail.com>
2016-06-23 09:50:03 +0800

    testing

--- GUI_rehearsal.py
+++ GUI_rehearsal.py
@@ -1,2 +1,19 @@
 from tkinter import *
 
+
+class App(object):
+    def __init__(self, root):
+        self.text = Text(root, relief='groove', width=6, height=3)
+        self.text.pack()
+        self.button = Button(root, text='Left', width=9, height=2, command=self.go).pack(side=LEFT)
+
+    def go(self):
+        self.text.delete("1.0", END)
+        self.text.insert(END, "do")
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry('400x300')
+    app = App(root)
+    root.mainloop()
